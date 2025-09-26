5cd93e8bf50f391117aa3b72f1bc6fa4e0283291
Ambiente 106 <ambiente106@iMac-de-CADPH.local>
2025-09-26 17:15:58 -0500

    actualizacion

---
+++ js/carrito.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+const botonComprar = document.querySelectorAll (".comprar");
+const carro= document.querySelectorAll ("carrito");
+const contador = document.querySelectorAll ("contador");
+
+let cantidad =0;
+
+botonComprar.forEach(boton => {
+    boton.addEventListener("Click", () =>{
+        cantidad++;
+
+        contador.textContent = cantidad;
+
+        carro.classList.add("mostar");
+        });
+    });
+});
